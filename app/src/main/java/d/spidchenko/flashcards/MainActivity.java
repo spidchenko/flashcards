af64a93acc870cfdf95cfb1fb6d86e3e2425497d
Dmitry Spidchenko <spidchenko.d@gmail.com>
2022-06-01 15:19:55 +0300

    Initial commit

---
+++ app/src/main/java/d/spidchenko/flashcards/MainActivity.java
@@ -0,0 +1,21 @@
+package d.spidchenko.flashcards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import d.spidchenko.flashcards.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
